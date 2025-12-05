7e967823b6c9237f82890a73850de8d9f986e407
Hentrixs <tobiasfala@gmail.com>
2025-12-05 13:19:59 -0300

    v8

--- Pendientes.md
+++ Pendientes.md
@@ -3,20 +3,15 @@ Para hacer esto sera necesario generar probablemente en la carpeta data
 un archivo llamado contactMessages.js, tambien sera necesario que de alguna forma
 agarremos el mismo message box del contacto, para esto podria usarse la ID.
 
-3. Arreglar tamanio de iconos hasta la resolucion 1080p, si es necesario agregar 
+2. Arreglar tamanio de iconos hasta la resolucion 1080p, si es necesario agregar 
 otra media query
 
-4. Arreglar los botones que van de (all - Groups) para que este seleccionado
-all y que se pueda cambiar a groups si es necesario
+3. Meter la hora de cuando se mando el ultimo mensaje en el contactlist
 
-5. Meter la hora de cuando se mando el ultimo mensaje en el contactlist
-
-6. Buscar como crear un CSS global donde pongamos los colores de la pagina usando ::root
-
-7. Preguntar a la IA como hacer todo esto mas profesional, en especial en lo que seria a estilos CSS.
-
-8. Para la app de celular (menos de 768px) buscar agregar distintas direcciones
+4. Para la app de celular (menos de 768px) buscar agregar distintas direcciones
 para los 3 iconos del menu principal
+
+5. Meter funcionalidad a los botones all - groups y arreglar el color
 
 9. Buscar cambiar los iconos de los contactos para que sean mas variados
 
--- src/Components/ContactSearchForm/ContactSearchForm.jsx
+++ src/Components/ContactSearchForm/ContactSearchForm.jsx
@@ -1,17 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ContactSearchForm.css'
 
 export default function ContactSearchForm() {
+  const [ButtonState,setButtonState] = useState([
+    {
+      id:1,
+      name:"all",
+      name_value:true
+    },
+    {
+      id:2,
+      name:"unread",
+      name_value:false
+    },
+    {
+      id:3,
+      name:"favourites",
+      name_value:false
+    },
+    {
+      id:4,
+      name:"groups",
+      name_value:false
+    }
+  ]);
+
+  const handleClick = (id) => {
+
+    setButtonState((prev) => {
+      return prev.map((item) => {
+        return item.id === id ?
+        {...item, name_value: true} :
+        {...item, name_value: false}
+      })
+    })
+  }
+  
+  console.log(ButtonState);
+
   return (
     <div className='contact-search-form'>
       <form action="#">
         <input type="text" placeholder='Search or start a new chat'/>
       </form>
       <div className='contact-buttons'>
-        <button>All</button>
-        <button>Unread</button>
-        <button>Favourites</button>
-        <button>Groups</button>
+        {ButtonState.map((button) => {
+          return(
+            <button key={button.id}
+            // Arreglar el color aca (black)
+            style={{backgroundColor: button.name_value ? "green" : "black"}}
+            onClick={() => handleClick(button.id)} 
+            >{button.name}
+            </button>
+          )
+        })}
       </div>
     </div>
   )
