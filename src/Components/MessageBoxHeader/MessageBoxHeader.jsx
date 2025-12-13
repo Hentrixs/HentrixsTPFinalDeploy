64393f66764aed14c9d324770bc48155c990df36
Hentrixs <tobiasfala@gmail.com>
2025-12-13 17:12:21 -0300

    v49

--- src/Components/MessageBoxHeader/MessageBoxHeader.jsx
+++ src/Components/MessageBoxHeader/MessageBoxHeader.jsx
@@ -1,18 +1,23 @@
-
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import './MessageBoxHeader.css'
-import { getContactById, getContactList } from '../../services/contactService'
+import { getContactById } from '../../services/contactService'
 import { MessageBoxContext } from '../../Contexts/MessageBoxContext/MessageBoxContext';
-import { useParams } from 'react-router';
+import { Link, useParams } from 'react-router';
 
 export default function MessageBoxHeader() {
 
   const {id} = useParams();
+  const {setMessageBoxText} = useContext(MessageBoxContext);
   const contact = getContactById(Number(id));
-
+  
   return (
     <div className='messagebox-header'>
       <div className='messagebox-div-left'>
+        <Link to={"/"}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+          </svg>    
+        </Link>
         <img src={contact.contact_avatar} alt="" />
         {
           <div>
